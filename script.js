ac43f38b9e58d1d023584b62b7a61854792fe357
Jack McCall <jackson.mccall907@gmail.com>
2024-09-18 12:53:20 -0500

    Added in filler content for Unity, LoL, and Valorant pages

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ function toggleResume() {
         resumeTitle.classList.toggle('open');
     }
 }
+
+function toggleContent(section) {
+    const content = document.getElementById(`${section}-content`);
+    const toggleBox = document.querySelector(`.toggle-box.${section}-title`);
+    
+    if (content) {
+        content.classList.toggle('active');
+        if (toggleBox) {
+            toggleBox.classList.toggle('open');
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.getElementById('main-header');
+
+    // Retrieve animation status from local storage
+    const animationState = localStorage.getItem('headerAnimationState');
+
+    // If there's a saved state, apply it
+    if (animationState) {
+        header.classList.add(animationState);
+    }
+
+    // Save the animation state when the page is about to be unloaded
+    window.addEventListener('beforeunload', () => {
+        localStorage.setItem('headerAnimationState', 'header-animation');
+    });
+});
