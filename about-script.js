c185a8a93105e100d2ae2ed3d9f7bc17c2f8d1f7
Jack McCall <jackson.mccall907@gmail.com>
2024-09-24 13:47:33 -0500

    Removed some bloat from about

--- about-script.js
+++ about-script.js
@@ -1,27 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const header = document.getElementById('main-header');
-
-    // Automatically open the resume section on page load
     const resumeContent = document.getElementById('resume-content');
     const resumeTitle = document.querySelector('.resume-title');
-    if (resumeContent) {
-        resumeContent.classList.add('active');
-        resumeTitle.classList.add('open');
-        resumeTitle.querySelector('.arrow').style.transform = 'rotate(180deg)';
-    }
-
-    // Retrieve animation status from local storage
-    const animationState = localStorage.getItem('headerAnimationState');
-
-    // If there's a saved state, apply it
-    if (animationState) {
-        header.classList.add(animationState);
-    }
-
-    // Save the animation state when the page is about to be unloaded
-    window.addEventListener('beforeunload', () => {
-        localStorage.setItem('headerAnimationState', 'header-animation');
-    });
+    resumeContent.classList.add('active');
+    resumeTitle.classList.add('open');
+    resumeTitle.querySelector('.arrow').style.transform = 'rotate(180deg)';
 });
 
 function toggleResume() {
--- index-script.js
+++ index-script.js
@@ -1,16 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const header = document.getElementById('main-header');
-
-    // Retrieve animation status from local storage
-    const animationState = localStorage.getItem('headerAnimationState');
-
-    // If there's a saved state, apply it
-    if (animationState) {
-        header.classList.add(animationState);
-    }
-
-    // Save the animation state when the page is about to be unloaded
-    window.addEventListener('beforeunload', () => {
-        localStorage.setItem('headerAnimationState', 'header-animation');
-    });
+    // Cleaned up because 'header' & related assignments not used in the HTML
 });
